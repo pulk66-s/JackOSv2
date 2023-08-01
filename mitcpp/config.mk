ca49b16034ac4b02c78a6d57d2c1fad37b8c82f9
pulk66-s <hugo.marrasse@epitech.eu>
2023-08-01 14:18:04 +0200

    feat: Add of working makefile that compiles all obj into obj/ folder

--- mitcpp/config.mk
+++ mitcpp/config.mk
@@ -4,25 +4,30 @@
 ##
 
 ## Global variables
-OS_NAME 	:= MITJackos
+OS_NAME 			:= MITJackos
 
 ## Echo COLORS
-GREEN 		:= \033[0;32m
-RED 		:= \033[0;31m
-NC 			:= \033[0m
+GREEN 				:= \033[0;32m
+RED 				:= \033[0;31m
+NC 					:= \033[0m
 
 ## Project folders
-SRC_DIR		:= src
-BOOT_DIR	:= $(SRC_DIR)/boot
+SRC_DIR				:= $(PROJECT_PWD)/src
+BOOT_NAME			:= boot
+BOOT_DIR			:= $(SRC_DIR)/$(BOOT_NAME)
+OBJ_DIR				:= $(PROJECT_PWD)/obj
+GENERATED_FOLDERS	:= $(OBJ_DIR)	# It's generated from the $(PROJECT_PWD) folder
 
 ## Project Submodules to build
-## PROJECT_PWD is set in the 
-SUBMODULES	:= $(PROJECT_PWD)/$(BOOT_DIR)
+## PROJECT_PWD is set in the main Makefile
+SUBMODULES			:= $(PROJECT_PWD)/$(BOOT_DIR)
 
-## Compilers
-CC			:= $(HOME)/cc/bin/i686-elf-g++
-AS			:= $(HOME)/cc/bin/i686-elf-as
+## Binaries
+CC					:= $(HOME)/cc/bin/i686-elf-g++
+AS					:= $(HOME)/cc/bin/i686-elf-as
+QEMU				:= qemu-system-i386
 
 ## Flags
-CFLAGS		:= -W -Wall -Wextra -Werror
-ASFLAGS		:=
+CFLAGS				:= -W -Wall -Wextra -Werror
+ASFLAGS				:=
+QEMU_FLAGS			:=
